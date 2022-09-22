2da94ea29bbb0228ad6cbbb78e988ec1870a2917
Victoria Coker <vic-e@Victorias-MacBook-Air.local>
2022-09-22 00:16:43 -0400

    added plant modal

--- best-buds/src/Footer.js
+++ best-buds/src/Footer.js
@@ -11,7 +11,7 @@ class Footer extends Component{
         <Col style={{textAlign: "left"}}> <h4>&copy; Copyright 2022</h4></Col>
         <Col style={{textAlign: "left"}}>
           <h4>Developers</h4>
-          <h5>Augustina Fin Scigliano  <i class="bi bi-github" style={{ fontSize: 20 }}></i> <i class="bi bi-linkedin" style={{ fontSize: 20 }}></i></h5>
+          <h5>Agustina Fin Scigliano  <i class="bi bi-github" style={{ fontSize: 20 }}></i> <i class="bi bi-linkedin" style={{ fontSize: 20 }}></i></h5>
           <h5>Sean Flanagan <i class="bi bi-github" style={{ fontSize: 20 }}></i> <i class="bi bi-linkedin" style={{ fontSize: 20 }}></i></h5>
           <h5>Victoria Coker <i class="bi bi-github" style={{ fontSize: 20 }}></i> <i class="bi bi-linkedin" style={{ fontSize: 20 }}></i></h5>
         </Col>
---
+++ best-buds/src/PlantModal.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { Modal, Button, Container, Row, Col, Image} from "react-bootstrap";
+
+class PlantModal extends Component {
+  state = {
+    isOpen: false
+  };
+
+  openModal = () => this.setState({ isOpen: true });
+  closeModal = () => this.setState({ isOpen: false });
+
+  render() {
+    return (
+      <>
+        <div className="d-flex align-items-center justify-content-center">
+          <Button variant="primary" onClick={this.openModal}>
+            Plant Name
+          </Button>
+        </div>
+        <Modal show={this.state.isOpen} onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Plant Name</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          <Container>
+            <Row>
+            <Col xs={6} md={4}>
+              <Image width={200} src="https://www.gardeningknowhow.com/wp-content/uploads/2008/05/rubber-plant.jpg" roundedCirle />
+            </Col>
+              <Col xs={12} md={8}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+                </Col>
+            </Row>
+            </Container>
+          </Modal.Body>
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default PlantModal;
+
+
+//got code from here https://www.pluralsight.com/guides/how-to-trigger-modal-for-react-bootstrap
