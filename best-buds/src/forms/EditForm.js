d04d4b96505394d18c4cfcd567c300b9157b6e91
goosefin <agustinafin96@gmail.com>
2022-09-24 13:24:56 -0400

    added the edit for to where the plants are bing mapped

--- best-buds/src/forms/EditForm.js
+++ best-buds/src/forms/EditForm.js
@@ -21,36 +21,31 @@ class AddPlantModal extends Component{
         })
     }
 
-    handleEdit = (plant) =>{
-        fetch('http://localhost:3003/plants', {
-            method:'POST',
-            body: JSON.stringify({
-                name:this.state.name,
-                img:this.state.img,
-                lightNeed:this.state.lightNeed,
-                waterNeed:this.state.waterNeed,
-                description:this.state.description,
-                calssification:this.state.classification,
-            }),
-            headers:{
-                'Content-Type':'application/json'
-            }
-        })
-        .then(res => res.json())
-        .then(resJson => {
-            console.log('NewPlant - resJson' + resJson)
-            this.handleAddPlant(resJson)
-            this.setState = ({
-                name:'',
-                img:'',
-                lightNeed:'',
-                waterNeed:'',
-                description:'',
-                classification:'',
-                username:''
-            })
-        })
-    }
+    // handleEdit = (plant) =>{
+    //     fetch('http://localhost:3003/plants' + plant._id, {
+    //         method:'PUT',
+    //         body: JSON.stringify({
+    //             name:this.state.name,
+    //             img:this.state.img,
+    //             lightNeed:this.state.lightNeed,
+    //             waterNeed:this.state.waterNeed,
+    //             description:this.state.description,
+    //             classification:this.state.classification,
+    //         }),
+    //         headers:{
+    //             'Content-Type':'application/json'
+    //         }
+    //     })
+    //     .then(res => res.json())
+    //     .then(resJson => {
+    //         const copyPlants = [...this.state.plants]
+    //         const findIndex = this.state.plants.findIndex(plant => plant._id === resJson.id)
+    //         copyPlants[findIndex] = resJson
+    //         this.setState = ({
+    //             plants:[copyPlants]
+    //         })
+    //     })
+    // }
 
 
     render(){
