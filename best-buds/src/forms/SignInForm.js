b4cda2c91260de65b35e91a9c087ecd3a93ff0e6
Victoria Coker <vic-e@Victorias-MacBook-Air.local>
2022-10-01 01:33:43 -0400

    Redesigned the sign-in form

--- best-buds/src/forms/SignInForm.js
+++ best-buds/src/forms/SignInForm.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Form, Button} from "react-bootstrap";
 
 class SignInForm extends Component{
     constructor(props){
@@ -41,25 +42,38 @@ class SignInForm extends Component{
         return(
             <>
                 <form onSubmit={this.handleSignin}>
-
-                    <label htmlFor="email">Email:</label>
-                    <input
+                <fieldset className="formDesign">
+                <h3 className="text-center d-block">Best Buds</h3>
+                    <Form.Group className="mb-3 mt-4">
+                    <Form.Label htmlFor="email"><h5>Email</h5></Form.Label>
+                    <Form.Control
                         type="email"
                         htmlFor="email"
                         id="email"
                         name="email"
                         onChange={this.props.handleChange}
                     />
+                    </Form.Group>
 
-                    <label htmlFor="password">Password:</label>
-                    <input
+                    <Form.Group className="mb-3">
+                    <Form.Label htmlFor="password"><h5>Password</h5></Form.Label>
+                    <Form.Control
                         type="password"
                         htmlFor="password"
                         id="password"
                         onChange={this.props.handleChange}
                     />
-                    <input type="submit" value="Sign In"/>
+                    </Form.Group>
+
+                    <div className="d-flex align-items-center justify-content-center">
+                    <Button className="buttonWidth" variant="success" size="lg" type="submit">
+                    Sign In
+                    </Button>
+                    </div>
+
+                    </fieldset>
                 </form>
+                <h5 className="mt-4">Don't have an account? <a style={{ color: '#333333'}}href={`/Register/`}>Register here.</a></h5>
             </>
         )
     }
