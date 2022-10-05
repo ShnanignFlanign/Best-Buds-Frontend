04e347d9a42eb8d0327b00cd9cc93fa7b767f5d6
Victoria Coker <vic-e@Victorias-MacBook-Air.local>
2022-10-05 13:37:11 -0400

    aligned the Section Header with add a plant button

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -41,13 +41,13 @@ class Welcome extends Component{
           </Col>
         </Row>
         <Row>
-       
-        
-        <Row>
-        <Col xs={12} md={6}>
+
+
+        <Row className="welcomeDesign">
+        <Col xs={6} md={6}>
           <h2>Your Buds</h2>
         </Col>
-        <Col xs={12} md={6} className="d-flex justify-content-end">
+        <Col xs={6} md={6} className="d-flex justify-content-end">
         {/* <Button variant="outline-success"> */}
           <AddPlantModal/>
         {/* </Button> */}
@@ -58,12 +58,12 @@ class Welcome extends Component{
         {/* should we be using seeded data and a separate modal */}
         {this.state.plants.map((plant) =>{
             if(plant.username === 'default'){
-              return (<Plant key={plant._id} name={plant.name} img={plant.img} lightNeed={plant.lightNeed} waterNeed={plant.waterNeed} descritpion={plant.description} classification={plant.classification}/>)
+              return (<Plant key={plant._id} name={plant.name} img={plant.img} lightNeed={plant.lightNeed} waterNeed={plant.waterNeed} description={plant.description} classification={plant.classification}/>)
             }
           })
         }
         </Row>
-        
+
         </Row>
 
       </div>
