f9a102eeedbd43c1e5ac05c2547f1300f6ef9001
goosefin <agustinafin96@gmail.com>
2022-10-08 12:46:34 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component{
       isLoggedIn:true, 
       username: content.user.username
     })
-  }
+  } 
 
   handleChange = (e) =>{
     this.setState({[e.target.id]: e.target.value})
